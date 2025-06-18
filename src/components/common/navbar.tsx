99dc4e10dd90b67495aa56c569a40692785fb737
vinitkesh <>
2025-06-18 20:00:00 +0530

    [fix-minor] navbar visibility - all pages

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -7,7 +7,7 @@ export interface NavbarProps {
 }
 
 export const Navbar = ({ variant }: NavbarProps) => (
-  <nav className={`flex justify-between items-center p-4 border-b shadow-sm ${variant === 'blue' ? 'bg-blue-400' : 'bg-white'}`}>
+  <nav className={`flex justify-between items-center p-4 border-b shadow-sm ${variant === 'blue' ? 'bg-blue-400' : 'bg-white'} text-black`}>
     <Link href={'/'} className="text-xl">Elevate NITC</Link>
     <div className="Links flex gap-3 ">
       <Link href={'/companies'} className={`font-sans ${variant==='blue' ?  'text-white ' : 'text-elevate-blue-400' }`}>Companies</Link>
@@ -15,9 +15,3 @@ export const Navbar = ({ variant }: NavbarProps) => (
     </div>
   </nav>  
 );
-
-const HamburgerMenu = () => {
-  <>
-
-  </>
-}
